ad3f8b86b025a2a32d70ea25e274e8383c2e6dc7
Shozis <144358159+Shozis@users.noreply.github.com>
2023-10-23 19:10:17 +0300

    lab 2 C#

---
+++ lab1_1_2/lab1.1.2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace lab1._1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double x = -2;
+            double y = 4;
+            double c = -4;
+            double d = 8;
+            double e = 1.0;
+
+            if (x * y > 3)
+            {
+                // Предположение
+
+                double maxEl = Math.Log10(Math.Pow(x * y, c * d));
+                if (maxEl > Math.Pow(x, 3))
+                {
+                    maxEl = Math.Pow(x, 3);
+                }
+
+                e = maxEl;
+            }
+            else if (0 < x * y && x * y < 3)
+            {
+                double maxEl = d * y;
+
+                if (maxEl > c * x)
+                {
+                    maxEl = c * x;
+                }
+
+                // Предположение
+
+                double minEl = x * 3;
+
+                if (minEl < y)
+                {
+                    minEl = y;
+                }
+
+                if (minEl < maxEl)
+                {
+                    minEl = maxEl;
+                }
+
+                e = maxEl;
+            }
+            else if (x * y < 0)
+            {
+                e = Math.Pow(2, c * d) - x;
+            }
+
+            Console.WriteLine(e);
+
+            // Пауза
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ lab1_3_2/lab1_3_2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1_3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double x = 5;
+            double b = -1;
+            double c = 9;
+            double h = 1.0;
+            if (0 < x && x < 1)
+            {
+
+                //Предположение
+
+                h = b * x + 1;
+            }
+            else if (x <= 0)
+            {
+
+                //Предположение
+
+                double minEl = Math.Sqrt(Math.Abs(b * x));
+                if (minEl < Math.Pow(x, 3))
+                {
+                    minEl = Math.Pow(x, 3);
+                }
+
+                h = Math.Pow(x, 3);
+
+                if (minEl < x + b)
+                {
+                    minEl = x + b;
+                }
+
+                h = x + b;
+            }
+            else
+            {
+
+                double maxEl = x + c;
+                if (maxEl > Math.Cos(b * x))
+                {
+                    maxEl = Math.Cos(b * x);
+                }
+
+                h = maxEl;
+            }
+
+            Console.WriteLine(h);
+
+            //Пауза
+            Console.ReadLine();
+        }
+    }
+}
