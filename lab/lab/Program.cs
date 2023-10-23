7d985b1e3182c8ea7e80d89acd5174f479729c3b
Shozis <144358159+Shozis@users.noreply.github.com>
2023-10-23 19:06:21 +0300

    lab 1 C#

---
+++ lab/lab/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("h = (xy + sin x)/(|1-y|*In x)");
+            Console.WriteLine("Введите x = ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите y = ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            double h = (x * y + Math.Sin(x)) / (Math.Abs(1 - y) * Math.Log(x));
+
+            double k = h;
+            Console.WriteLine("результат вычислений =");
+            Console.WriteLine(k);
+
+            double n = Math.Truncate(h);
+            Console.WriteLine("целая часть=");
+            Console.WriteLine(n);
+
+
+
+        }
+    }
+}
