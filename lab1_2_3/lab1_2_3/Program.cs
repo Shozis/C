fe0e000bd92c1ba921fd7aa19fbe350f78a701e4
Shozis <144358159+Shozis@users.noreply.github.com>
2023-10-23 19:53:46 +0300

    Revert "Revert "lab 2 C++""
    This reverts commit a719eab9ad4ddcd3bb7ab1baefae52cc33435107.

---
+++ lab1_2_3/lab1_2_3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace lab1_2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double y;
+            double x = 0;
+            double i = -Math.PI;
+
+            //while
+
+            while (i < Math.PI)
+            {
+                y = 3 * Math.Pow(Math.Cos(2 * x + 1), 2);
+                i += Math.PI / 8;
+                Console.WriteLine(y);
+            }
+    }
+}
