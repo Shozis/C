e0ea45da969c4c702381ed8e5d7a8a7772d3732a
Shozis <144358159+Shozis@users.noreply.github.com>
2023-10-23 19:12:10 +0300

    lab 2 C++

---
+++ lab2_3_2/lab2_3_2/lab2_3_2.cpp
@@ -0,0 +1,44 @@
+﻿// lab2_3_2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    
+	int x = -3;
+	int b = 6;
+	int c = -4;
+
+	int h;
+
+	if (0 < x && x < 1) {
+
+		h = b * x + 1;
+	}
+	else if (x <= 0) {
+
+		int minEl = pow(x, 3);
+		if (minEl < sqrt(abs(b * x))) {
+			minEl = sqrt(abs(b * x));
+		}
+
+		if (minEl < x + b) {
+			minEl = x + b;
+		}
+
+		h = minEl;
+	}
+	else {
+
+		int maxEl = cos(b * x);
+		if (maxEl > x + c) {
+			maxEl = x + c;
+		}
+	}
+
+	cout << h << endl;
+}
+
